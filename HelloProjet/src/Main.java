8cbc6796743cbfac7e955ac9e71f970f1f11c519
813069174 <813069174@qq.com>
2019-08-06 19:31:23 +0800

    李四提交的代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+﻿import org.junit.Test;
 
 public class Main {
 
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("提交新的修改");
+        System.out.println("李四提交新的修改");
     }
 }
